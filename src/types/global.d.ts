9667d88ed89b921f8ca69dd1a2343e6edea39763
alexey-ryabkov <xelaris@ya.ru>
2025-10-30 17:36:32 +0300

    chore: svg tackling, fix pkgs ver

---
+++ src/types/global.d.ts
@@ -0,0 +1,4 @@
+declare module "virtual:svg-icons-register" {
+  const value: unknown;
+  export default value;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { fileURLToPath, URL } from "node:url";
+import path from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgLoader from "vite-svg-loader";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), tsconfigPaths(), vueDevTools()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    tsconfigPaths(),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+      symbolId: "[name]",
+    }),
+    svgLoader(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
